7d6c98727a0495d20d04fa1d328dc6c87446e15f
Gustavo Henrique <84508964+guustavo07@users.noreply.github.com>
2024-02-05 13:28:33 -0300

    Add files via upload

---
+++ Publisher/Publisher/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection())
+                
+            using(var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "saudacao_1",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "Bem-vindo ao rabbitMQ";
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "saudacao_1",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine($" [x] Enviada : {message}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
